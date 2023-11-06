37531e8de38d684a5d726a3fb25429eb8eb6e0f0
xvishnevskix <45593711+xvishnevskix@users.noreply.github.com>
2023-11-06 13:16:09 +0400

    first

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<div>
+  <div class="post" v-for="post in posts">
+    <div><strong>Название:</strong> {{posts.title}}</div>
+    <div><strong>Описание:</strong> {{posts.body}}</div>
+  </div>
+</div>
 </template>
 
+
+<script>
+  export default {
+    data() {
+      return {
+        posts: [
+          {id: 1, title: 'JavaScript', body: 'Описание поста'},
+          {id: 2, title: 'JavaScript', body: 'Описание поста 2'},
+          {id: 3, title: 'JavaScript', body: 'Описание поста 3'},
+        ]
+      }
+    },
+    methods: {
+      addLike() {
+        this.likes += 1;
+      },
+      addDislikes() {
+        this.dislikes += 1;
+      }
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    *{
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+    .post {
+      margin: 15px;
+      padding: 15px;
+      border: 2px solid teal;
+    }
 </style>
+
